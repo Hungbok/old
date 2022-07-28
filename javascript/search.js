d64d227a3aaf084fb92747904fa051388c1180d8
Hungbok <bjminimn@naver.com>
2022-07-28 17:47:52 +0900

    Update

--- javascript/search.js
+++ javascript/search.js
@@ -1,5 +1,21 @@
-$(function (){
-    $("#myInput").click(function (){
-        $("#myUL").toggle();
-    });
-});
+function filter(){
+  
+    var value, name, item, i;
+
+    value = document.getElementById("value").value.toUpperCase();
+    item = document.getElementsByClassName("history-item");
+
+    for(i=0;i<item.length;i++){
+      name = item[i].getElementsByClassName("name");
+      if(name[0].innerHTML.toUpperCase().indexOf(value) > -1){
+        item[i].style.display = "inline-block";
+      }else{
+        item[i].style.display = "none";
+      }
+    }
+  }
+
+  $(".scale").click(function(){
+    $('.headline').toggle({'display': 'none'});
+    $('.agenda').toggle({'display': 'inline-block'});
+  });
