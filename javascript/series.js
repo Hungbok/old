00bb5c3d4af259f328ef5379dd156a858fe3a496
Hungbok <bjminimn@naver.com>
2022-08-05 21:44:59 +0900

    Update

---
+++ javascript/series.js
@@ -0,0 +1,17 @@
+$(".headline").click(function(){
+  $('.series').css({'height': '110px'});
+  $('.item').css({'width': '97px'});
+  $('.item-thumbnail').css({'height': '145.5px'});
+  $('.item-thumbnail').css({'width': '97px'});
+  $('.headline').css({'display': 'none'});
+  $('.agenda').css({'display': 'inline-block'});
+});
+
+$(".agenda").click(function(){
+  $('.series').css({'height': 'auto'});
+  $('.item').css({'width': '200px'});
+  $('.item-thumbnail').css({'height': '300px'});
+  $('.item-thumbnail').css({'width': '200px'});
+  $('.headline').css({'display': 'inline-block'});
+  $('.agenda').css({'display': 'none'});
+});
